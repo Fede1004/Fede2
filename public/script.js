0c2b919ede26b268574c90e0a9bca58b88166349
Fede1004 <federico.olivieri1999@gmail.com>
2024-06-11 17:12:36 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,14 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const imageInput = document.getElementById('imageInput');
     const promptInput = document.getElementById('aiPrompt');
     const resultImage = document.getElementById('resultImage');
-    const progressBar = document.getElementById('progressBar');
     const errorDisplay = document.getElementById('errorDisplay');
 
     submitButton.addEventListener('click', function(event) {
         event.preventDefault();
-        progressBar.style.width = '0%';  
-        errorDisplay.textContent = '';  
-
         if (!imageInput.files.length) {
             displayError('Please select an image to upload.');
             return;
@@ -24,17 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
             method: 'POST',
             body: formData
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error, status = ${response.status}`);
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            if (data.jobId) {
-                checkJobStatus(data.jobId);
+            if (data.imageUrl) {
+                resultImage.innerHTML = `<img src="${data.imageUrl}" alt="Edited Image" style="width:100%;">`; // Mostra l'immagine a schermo intero
             } else {
-                throw new Error('Failed to submit image for processing.');
+                throw new Error(data.error || 'Failed to submit image for processing.');
             }
         })
         .catch(error => {
@@ -42,27 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    function checkJobStatus(jobId) {
-        fetch(`/job-status/${jobId}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.state === 'completed' && data.result) {
-                resultImage.innerHTML = `<img src="${data.result}" alt="Edited Image"/>`;
-                progressBar.style.width = '100%';
-            } else if (data.state === 'failed') {
-                throw new Error('Failed to process the image.');
-            } else {
-                progressBar.style.width = `${parseInt(progressBar.style.width) + 10}%`; // Incremental progress
-                setTimeout(() => checkJobStatus(jobId), 3000); // Repeat polling every 3 seconds
-            }
-        })
-        .catch(error => {
-            displayError(`An error occurred: ${error.message}`);
-        });
-    }
-
     function displayError(message) {
         errorDisplay.textContent = message;
-        progressBar.style.width = '0%';
     }
 });
